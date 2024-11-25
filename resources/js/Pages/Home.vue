5a8d7d93c3b8b9a95dab8c79cd0b7e7b07d7d168
taufikcharlie <mohamadtaufik099@gmail.com>
2024-11-25 11:59:02 +0800

    feat: dropdown components , alias resolve

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,11 +1,50 @@
+<script setup>
+import { useForm } from '@inertiajs/vue3'
+import TextInput from '@Components/TextInput.vue'
+import Dropdown from '@Components/Dropdown.vue';
+
+const form = useForm({
+    email: null,
+    password: null,
+    remember: null,
+    dropdown: null, // Added this for dropdown binding
+});
+
+const items = ['key','fork','spoon']
+</script>
+
 <template>
     home vue , laravel , inertia , tailwind
     <div>
-  <p class="text-slate-500">
-    I’m Derek, an astro-engineer based in Tattooine. I like to build X-Wings at
-    <a class="underline decoration-sky-500/30">My Company, Inc</a>.
-    Outside of work, I like to <a class="underline decoration-pink-500/30">watch
-    pod-racing</a> and have <a class="underline decoration-indigo-500/30">light-saber</a> fights.
-  </p>
-</div>
+        <p class="text-slate-500">
+            I’m Derek, an astro-engineer based in Tattooine. I like to build X-Wings at
+            <a class="underline decoration-sky-500/30">My Company, Inc</a>.
+            Outside of work, I like to <a class="underline decoration-pink-500/30">watch
+            pod-racing</a> and have <a class="underline decoration-indigo-500/30">light-saber</a> fights.
+        </p>
+    </div>
+
+    <div>
+        <TextInput
+            labelName="Email"
+            type="email"
+            v-model="form.email"
+            :message="form.errors.email"
+        />
+        <TextInput
+            labelName="Email"
+            type="email"
+            v-model="form.email"
+            :message="form.errors.email"
+        />
+    </div>
+    <div>
+        <Dropdown
+            labelName="Select an Item"
+            name="Please select"
+            :items="items"
+            v-model="form.dropdown"
+            :message="form.errors.dropdown"
+        />
+    </div>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
@@ -10,4 +11,10 @@ export default defineConfig({
         }),
         vue(),
     ],
+    resolve: {
+        alias: {
+            '@Components': resolve(__dirname, 'resources/js/Components'),
+            '@Pages': resolve(__dirname, 'resources/js/Pages'),
+        },
+    },
 });
